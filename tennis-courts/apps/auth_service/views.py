2182a62ac30f8055814eb019495fb9a21bfe0788
mban7 <banasikm07@gmail.com>
2026-01-10 12:25:44 +0100

    feat: refresh token endpoint

--- tennis-courts/apps/auth_service/views.py
+++ tennis-courts/apps/auth_service/views.py
@@ -1,7 +1,10 @@
 from drf_spectacular.utils import extend_schema
+from jwt import ExpiredSignatureError
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import LoginSerializer
 from .services import login_user
@@ -63,4 +66,52 @@ class AuthView(ViewSet):
         response.delete_cookie("access_token", path="/api")
         response.delete_cookie("refresh_token", path="/auth/refresh")
         return response
+
+    @extend_schema(
+        request=None,
+        responses={200: MessageSerializer},
+        summary="Refresh token",
+    )
+    @action(detail=False, methods=["post"], url_path="refresh")
+    def refresh(self, request):
+        refresh_token = request.COOKIES.get("refresh_token")
+
+        if not refresh_token:
+            return Response({"detail": "Refresh token is missing"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            token = RefreshToken(refresh_token)
+        except ExpiredSignatureError:
+            return Response({"detail": "Token expired"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        new_access_token = token.access_token
+        new_refresh_token = token
+
+        response = Response({"detail": "ok"})
+
+        response.set_cookie(
+            "access_token",
+            new_access_token,
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            max_age=300,
+            path="/api"
+        )
+
+        response.set_cookie(
+            "refresh_token",
+            new_refresh_token,
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            max_age=86400,
+            path="/auth/refresh"
+        )
+
+        return response
+
+
+
+
 
