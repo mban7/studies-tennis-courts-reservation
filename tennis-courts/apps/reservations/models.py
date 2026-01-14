5cc01903e194fe27998ca3ee6035d0b9848be7cf
mban7 <banasikm07@gmail.com>
2026-01-14 19:42:49 +0100

    db changes and migrations

--- tennis-courts/apps/reservations/models.py
+++ tennis-courts/apps/reservations/models.py
@@ -3,8 +3,7 @@ import uuid
 from django.db import models
 from django.core.validators import MinValueValidator
 
-from apps.courts.models import Court
-from apps.payments.models import Payment
+from apps.courts.models import Court, CourtPrice
 from apps.users.models import User
 
 
@@ -16,12 +15,13 @@ class Reservation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     court = models.ForeignKey(Court, on_delete=models.PROTECT, related_name='reservation_court')
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='reservation_user')
-    payment = models.ForeignKey(Payment, on_delete=models.PROTECT, related_name='reservation_payment')
     players_count = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1)]
     )
     additional_info = models.TextField()
     status = models.CharField(max_length=10, choices=ReservationStatus.choices, default=ReservationStatus.PENDING)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    currency = models.CharField(max_length=8, choices=CourtPrice.Currency.choices, default=CourtPrice.Currency.PLN)
     start_at = models.DateTimeField()
     end_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
